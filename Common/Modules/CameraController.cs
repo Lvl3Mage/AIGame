5393959412fba3fb839756dc7ec4bed03b9042bf
Lvl3Mage <lvlthreemage@gmail.com>
2025-09-30 01:55:34 +0200

    ahaha

---
+++ Common/Modules/CameraController.cs
@@ -0,0 +1,18 @@
+﻿using Godot;
+
+namespace Game.Common.Modules;
+
+public partial class CameraController : Camera2D
+{
+	[Export] Node2D target;
+	[Export] float smoothingSpeed = 5f;
+
+	public override void _Process(double delta)
+	{
+		if (target == null) return;
+
+		float weight = 1f - Mathf.Exp(-smoothingSpeed * (float)delta);
+		GlobalPosition = GlobalPosition.Lerp(target.GlobalPosition, weight);
+	}
+
+}
