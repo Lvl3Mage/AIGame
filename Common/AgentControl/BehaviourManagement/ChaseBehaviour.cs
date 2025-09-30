f0c04368b79d0215820e12e97822de8954863c2c
Jeruko <jeruizko10@gmail.com>
2025-09-30 20:09:45 +0200

    Chase behaviour start

--- Common/AgentControl/BehaviourManagement/ChaseBehaviour.cs
+++ Common/AgentControl/BehaviourManagement/ChaseBehaviour.cs
@@ -1,4 +1,5 @@
 using Game.Common.AgentControl.Navigation;
+using Game.Common.Modules;
 using Godot;
 
 namespace Game.Common.AgentControl.BehaviourManagement;
@@ -6,59 +7,59 @@ namespace Game.Common.AgentControl.BehaviourManagement;
 /// <remarks>
 ///	Todo: Refactor this to use IAgentBehaviour properly and to use the <see cref="GridNavigation"/>
 /// </remarks>
-public partial class ChaseBehaviour : Node//, IAgentBehaviour
+public partial class ChaseBehaviour : Node, IAgentBehaviour
 {
-	//    [Export] TopdownMovementModule movementModule;
-	//
-	//    public BehaviourPriority Priority { get; set; } = BehaviourPriority.High;
-	// GameManager manager;
-	// NavMesh mesh;
-	//    Node2D target, root;
-	//    Vector2I[] path = [];
-	//    Vector2I nextPos;
-	//    int chaseIndex = 1;
-	//    float updateRate = 0.4f;
-	//
-	//    public override void _Ready()
-	//    {
-	//        root = GetOwner<Node2D>();
-	//        manager = GetTree().CurrentScene as GameManager;
-	//        mesh = manager.Mesh;
-	//        target = manager.Player;
-	//        UpdateChasePath();
-	//        CreateTimer();
-	//    }
-	//
-	//    public override void _Process(double delta)
-	//    {
-	//        // if (nextPos
-	//    }
-	//
-	//    void UpdateChasePath()
-	//    {
-	//        if (path.Length > chaseIndex)
-	//        {
-	//            // Advance one cell in the path
-	//            CellPosition = path[chaseIndex];
-	//        }
-	//
-	//        float width = mesh.CellSize / 2;
-	//        Vector2I currentCell = CellPosition;
-	//        Vector2I targetCell = mesh.WorldToGrid(target.Position + new Vector2(width, width));
-	//
-	//        manager.ClearPath(path);
-	//        path = mesh.GetShortestPathBFS(currentCell, targetCell, manager.Walls, true);
-	//        manager.PaintPath(path);
-	//    }
-	//
-	//    void CreateTimer()
-	//    {
-	//        Timer chaseTimer = new();
-	//        AddChild(chaseTimer);
-	//        chaseTimer.WaitTime = updateRate;
-	//        chaseTimer.Autostart = true;
-	//        chaseTimer.OneShot = false;
-	//        chaseTimer.Timeout += UpdateChasePath;
-	//        chaseTimer.Start();
-	//    }
+	[Export] TopdownMovementModule movementModule;
+
+	public IAgentBehaviour.Priority Priority { get; set; } = IAgentBehaviour.Priority.High;
+	GameManager manager;
+	GridNavigation grid;
+	Node2D target, root;
+	Vector2I[] path = [];
+	Vector2I nextPos;
+	int chaseIndex = 1;
+
+	public override void _Ready()
+	{
+		grid = GameManager.
+		target = manager.Player;
+	}
+	
+	   public override void _Process(double delta)
+	   {
+		UpdateChasePath();
+	   }
+	
+	   void UpdateChasePath()
+	   {
+	       if (path.Length > chaseIndex)
+	       {
+				
+	        	// Advance one cell in the path
+				CellPosition = path[chaseIndex];
+	       }
+	
+	       float width = mesh.CellSize / 2;
+	       Vector2I currentCell = CellPosition;
+	       Vector2I targetCell = mesh.WorldToGrid(target.Position + new Vector2(width, width));
+	
+	       manager.ClearPath(path);
+	       path = mesh.GetShortestPathBFS(currentCell, targetCell, manager.Walls, true);
+	       manager.PaintPath(path);
+	   }
+
+    public IAgentBehaviour.Priority GetPriority()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void StartBehavior()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void StopBehavior()
+    {
+        throw new System.NotImplementedException();
+    }
 }
