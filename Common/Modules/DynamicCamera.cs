2fc5bbf2e66d60c1bdbfa7fc08ce8b322390ecf9
Jeruko <jeruizko10@gmail.com>
2025-10-07 19:24:52 +0200

    Key, door, camera.

---
+++ Common/Modules/DynamicCamera.cs
@@ -0,0 +1,22 @@
+﻿using Godot;
+using System;
+
+[GlobalClass]
+public partial class DynamicCamera : Camera2D
+{
+	[Export] public float MouseInfluence = 0.1f;
+	[Export] public float SmoothSpeed = 3f;
+
+	private Vector2 targetOffset = Vector2.Zero;
+
+	public override void _Process(double delta)
+	{
+		Vector2 mousePos = GetViewport().GetMousePosition();
+		Vector2 screenCenter = GetViewport().GetVisibleRect().Size / 2;
+
+		Vector2 offset = (mousePos - screenCenter) * MouseInfluence;
+
+		targetOffset = targetOffset.Lerp(offset, (float)(SmoothSpeed * delta));
+		Position = targetOffset;
+	}
+}
