1f8836898630b6e557f71697f86c4f6dc4c0df4e
Salix <saulpachecotrilles@gmail.com>
2025-10-19 14:23:47 +0200

    import images

---
+++ Common/Modules/AnimationSprites2D.gd
@@ -0,0 +1,15 @@
+// extends AnimatedSprite2D
+using System;
+using Game.Common.Utility;
+using Godot;
+
+namespace Game.Common.Modules;
+
+[GlobalClass]
+public partial class AnimatedSprite : AnimatedSprite2D
+{
+    public override void _Process(double delta)
+    {
+
+    }
+}
