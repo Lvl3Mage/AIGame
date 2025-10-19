56cd6d5d44633a310d90439f54c47569cc6e0185
Jeruko <jeruizko10@gmail.com>
2025-10-19 14:52:18 +0200

    Player lose condition fixed

--- Common/Modules/LoseConditionModule.cs
+++ Common/Modules/LoseConditionModule.cs
@@ -1,4 +1,5 @@
 using Godot;
+using System;
 using System.Linq;
 
 namespace Game.Common.Modules;
@@ -6,28 +7,18 @@ namespace Game.Common.Modules;
 [GlobalClass]
 public partial class LoseConditionModule : Node
 {
-    [Export] CharacterBody2D agentBody;
+    [Export] Area2D damageArea;
     bool isPlayer = false;
 
-    public override void _PhysicsProcess(double delta)
+    public override void _Ready()
     {
-        // for (KinematicCollision2D collision = agentBody.GetSlideCollision(0); collision != null; collision = collision.Next)
-        for (int i = 0; i < agentBody.GetSlideCollisionCount(); i++)
-        {
-            KinematicCollision2D collision = agentBody.GetSlideCollision(i);
-            if (collision.GetCollider() is Node2D collider)
-            {
-                if (collider.IsInGroup("player"))
-                {
-                    isPlayer = true;
-                    GetTree().ReloadCurrentScene();
-                    break;
-                }
-                else
-                {
-                    isPlayer = false;
-                }
-            }
-        }
+        damageArea.BodyEntered += OnBodyEntered;
+    }
+
+    void OnBodyEntered(Node2D body)
+    {
+        GD.Print("CollisioN!");
+        if (body is PlayerController)
+            GetTree().ReloadCurrentScene();
     }
 }
