1b9b51186881f9fc85f4c586894de8d924fd3536
Jeruko <jeruizko10@gmail.com>
2025-10-19 02:10:58 +0200

    Added player sprite flip

--- Common/PlayerController.cs
+++ Common/PlayerController.cs
@@ -5,12 +5,28 @@ namespace Game.Common;
 public partial class PlayerController : CharacterBody2D
 {
     [Export] Modules.TopdownMovementModule movementModule;
+    [Export] Sprite2D sprite;
+
+    bool pressRight, pressLeft, pressUp, pressDown;
 
     public override void _Process(double delta)
     {
-        movementModule.InputRight = Input.IsActionPressed("moveRight");
-        movementModule.InputLeft = Input.IsActionPressed("moveLeft");
-        movementModule.InputUp = Input.IsActionPressed("moveUp");
-        movementModule.InputDown = Input.IsActionPressed("moveDown");
+        MapActions();
+
+        movementModule.InputRight = pressRight;
+        movementModule.InputLeft = pressLeft;
+        movementModule.InputUp = pressUp;
+        movementModule.InputDown = pressDown;
+
+        if (pressRight) sprite.FlipH = false;
+        else if (pressLeft) sprite.FlipH = true;
+    }
+
+    void MapActions()
+    {
+        pressRight = Input.IsActionPressed("moveRight");
+        pressLeft = Input.IsActionPressed("moveLeft");
+        pressUp = Input.IsActionPressed("moveUp");
+        pressDown = Input.IsActionPressed("moveDown");
     }
 }
